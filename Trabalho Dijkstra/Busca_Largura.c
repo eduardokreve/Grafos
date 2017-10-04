9a3fbf0c3f0a0d295139c83827b5a441d8b7d743
eduardokreve <eduardokreve283@gmail.com>
2017-10-04 14:16:03 -0300

    Função de BFS

--- Trabalho Dijkstra/Busca_Largura.c
+++ Trabalho Dijkstra/Busca_Largura.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define cor 0  /*0 == preto, 1 == cinza, 2 == preto */
 #define tam 21
 #define coluna_tab 4
+/*Cores Definidas: 0 == branco, 1 == cinza, 2 == preto */
+
+/*Implementacao da funcao de busca em largura*/
+void BFS(int tab[][tam], int matriz[][tam]) { //tabela das distancias e matriz de adjacencia
+    int i, j, cont = 0;
+
+    while(tab[2][20] == 0) { //enquanto o ultimo vertice continuar branco
+
+    }
+
+
+
+}
+
 
 int main() {                 /*a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, z*/
     int matriz_a[tam][tam] = {{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//a
@@ -33,14 +45,11 @@ int main() {                 /*a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q
                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//cor
                                    {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}};//vert. atual
 
-    
+    /*declaração do primeiro vertice(origem)*/
+    tabela[0][0] = 0; //distancia
+    tabela[2][0] = 1; //pinta de cinza
 
-
-
-
-
-
-
+    BFS(tabela, matriz_a); //funcao de busca em largura
 
 
     return 0;
