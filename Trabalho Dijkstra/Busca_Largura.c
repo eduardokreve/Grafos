b8dd0a506ffc3f49781d77f03986f3cc7d892f43
eduardokreve <eduardokreve283@gmail.com>
2017-10-04 21:10:56 -0300

    Função Gambi

--- Trabalho Dijkstra/Busca_Largura.c
+++ Trabalho Dijkstra/Busca_Largura.c
@@ -6,41 +6,56 @@
 #define cinza 1
 #define preto 2 /*Cores Definidas: 0 == branco, 1 == cinza, 2 == preto */
 
+void gambi(int vetor[], int vert) { //é uma gambiarra mesmo
+    int i = 0;
+
+    while(1) {
+        if(vetor[i] == -1) {
+            vetor[i] = vert;
+            break;
+        }
+        i++;
+    }
+}
 
 /*Implementacao da funcao de busca em largura*/
-void BFS(int tab[][tam], int matriz[][tam]) { //tabela das distancias e matriz de adjacencia
-    int i, j, cont = -1, anterior;
+void BFS(int tab[][tam], int matriz[][tam], int vertice, int vertices[]) { //faz o mapeamento
+    int j = 0;
 
+    for(j = 0; j < tam; j++) { //anda pelas colunas da matriz
+        if(matriz[vertice][j] == 1 && tab[2][j] != preto) { //se é ligado e não foi visitado
 
+            if(tab[2][j] == branco) tab[2][j] = cinza; //colore
+            else tab[2][j] = preto;
 
-    for(i = 0; i < tam-1; i++) { //i--> vertice
-        for(j = 0; j < tam-1; j++) { //j-->vertice que ele esta ligado
+            tab[0][j] = j; //distancia
+            tab[1][j] = j-1; //vertice anterior
 
-            if(matriz[i][j] == 1 && tab[2][j] != preto) { //se é ligado e não foi visitado
-                tab[0][j] = cont;
-
-                if(tab[2][j] == branco) tab[2][j] = cinza; //colore
-                else tab[2][j] = preto;
-
-                if(i == 0) anterior = 0; //qual o vertice anterior
-                else anterior = i-1;
-
-                tab[1][j] = anterior;
-            }
+            gambi(vertices, j);
         }
-        cont++;
+    }
+}
+
+void procura(int tab[][tam], int matriz[][tam]) { //procura onde mapear
+    int v_atual = 0;
+    int aux[4] = {-1,-1,-1,-1};
+
+    while(tab[3][tam] == branco) { //enquanto não pintar o ultimo vertice de outra cor
+        BFS(tab, matriz, v_atual, aux);
+        printf("%d %d %d %d\n", aux[0], aux[1], aux[2], aux[3]);
+        
     }
 }
 
 void imprime_tabela(int tabela[][tam]) {
     printf("Vertices:  A B C D E F G H I J K L M N O P Q R S T Z\n");
-    for(int i = 0; i < coluna_tab-1; i++) {
+    for(int i = 0; i < coluna_tab -1; i++) {
 
         if(i == 0)printf("distancia: ");
         else if(i == 1) printf("vert.ante: ");
         else if(i == 2) printf("Cor      : ");
 
-        for(int j = 0; j < tam-1; j++) {
+        for(int j = 0; j < tam; j++) {
             printf("%d ", tabela[i][j]);
         }
         printf("\n");
@@ -76,11 +91,11 @@ int main() {                 /*a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q
                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//cor
                                    {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}};//vert. atual
 
-    /*declaração do primeiro vertice(origem)*/
+    /*declaração do primeiro vertice(origem)
     tabela[0][0] = 0; //distancia
     tabela[2][0] = cinza; //pinta de cinza
-
-    BFS(tabela, matriz_a); //funcao de busca em largura
+    */
+    procura(tabela, matriz_a); //funcao de busca em largura
     imprime_tabela(tabela);
 
 
