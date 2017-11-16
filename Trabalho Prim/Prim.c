07a30855e4d52cab962da1067e1cec86b268be9c
eduardokreve <eduardokreve283@gmail.com>
2017-11-16 14:25:16 -0200

    Backup, vou alterar a logica

--- Trabalho Prim/Prim.c
+++ Trabalho Prim/Prim.c
@@ -2,32 +2,39 @@
 #include <stdlib.h>
 #define tam 8
 
-
-int contGrau(int matriz[][tam], int vertice) { //contar qual o grau de cada vertice
-	int i, num = 0;
+//verifica qual o grau do vertice e sua posicao
+void grauVertices(int matriz[][tam], int n, int vertices[tam]) {
+	int i, j = 0;
 
 	for(i = 0; i < tam; i++) {
-		if(matriz[vertice][i] != 0) num++;
+		if(matriz[n][i] != 0) {
+			vertices[j] = i; //recebe os vertices em que 'n' esta ligado
+			j++;
+		}
 	}
-	return num;
 }
 
-                                               //inic = orig
-void prim(int matriz[][tam], int table[][tam], int inic) { //recebe as matrizes e o primeiro vertice
+//recebe as matrizes e o primeiro vertice          inic = orig
+void prim(int matriz[][tam], int table[][tam], int inic) {
 	int i, j, menorPeso, primeiro;
 
 	while (1) {
 		primeiro = 1;
 
-		for (i = 0; i < tam; i++) {
-			if(table[1][i] != -1) {
-				int grau = contGrau(matriz, i);
+		for (i = 0; i < tam; i++) { //percorre todos os vertices
+			int vert[tam] = {-1,-1,-1,-1,-1,-1,-1,-1};
 
-				for(j = 0; j < grau; j++) {
+			if(table[1][i] != -1) { //e os vertices visitados, começando pelo 0
+				grauVertices(matriz, i, vert); //pega o grau dos vertices que ja foram visitados
 
-				}
+				
+
+
+
 			}
+			break;
 		}
+	break;
 	}
 }
 
@@ -43,8 +50,8 @@ int main() {                  //0  1  2  3  4  5  6  7
 
 
 					    //0  1   2   3   4   5   6   7
-	int table[2][tam] = {{0, 10, 10, 10, 10, 10, 10, 10},
-						 {0, -1, -1, -1, -1, -1, -1, -1}};
+	int table[2][tam] = {{0, 10, 10, 10, 10, 10, 10, 10}, //dist
+						 {0, -1, -1, -1, -1, -1, -1, -1}}; //vert anteriores
 
 
     prim(matrizAdj, table, 0);
